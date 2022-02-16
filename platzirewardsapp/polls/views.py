3540dea9e10e256ff0d610e65e82aa6f507a1390
Arturo0911 <anegreiross@outlook.com>
2022-02-16 12:24:34 -0500

    Finished django views, starting for django forms

--- platzirewardsapp/polls/views.py
+++ platzirewardsapp/polls/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 # Create your views here.
 
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("<h1>Hello motherFuckers!! :) </h1>")
+    latest_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {
+        "latest_question_list":latest_question_list
+    })
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question":question})
+
+
+def results(request, question_id):
+    return HttpResponse(f"You are viewing the results number {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You are voting for the question number {question_id}")
+
